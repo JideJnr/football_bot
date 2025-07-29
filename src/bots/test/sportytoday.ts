ce789112fc474119c2939b7e2fef983d8e5b9488
Olajide Nurudeen <84381571+JideJnr@users.noreply.github.com>
2025-07-29 01:44:13 +0100

    commit

--- src/bots/test/sportytoday.ts
+++
@@ -1,9 +0,0 @@
-import {  fetchTodayMatches } from '../../runners/sportybet';
-
-export async function today() {
-  try {
-    const data: any[] = await fetchTodayMatches();
-  } catch (error) {
-    console.error('Pipeline failed:', error);
-  }
-}
